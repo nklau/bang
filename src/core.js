52d5f7d1dc7a5a99895e0c0630dceceaa4cb4258
nklau <63122247+nklau@users.noreply.github.com>
2023-01-27 17:30:20 -0800

    can no longer assign 7 = 3

--- src/core.js
+++ src/core.js
@@ -4,77 +4,8 @@ export class Program {
   }
 }
 
-export class VariableDeclaration {
-  constructor(name, value) {
-    Object.assign(this, { name, value })
+export class BangFunc {
+  constructor(statements) {
+    this.statements = statements
   }
 }
-
-export class FunctionDeclaration {
-  constructor(params, body) {
-    Object.assign(this, { params, body })
-  }
-}
-
-// TODO: is this needed? think this belongs to number class
-export class Repeat {
-  constructor(times, body) {
-    Object.assign(this, { times, body })
-  }
-}
-
-// TODO: is this needed?
-export class Assignment {
-  constructor(target, source) {
-    Object.assign(this, { target, source })
-  }
-}
-
-export class PrintStatement {
-  constructor(expression) {
-    Object.assign(this, { expression })
-  }
-}
-
-export class Call {
-  constructor(callee, args) {
-    Object.assign(this, { callee, args })
-  }
-}
-
-export class Conditional {
-  constructor(condition, consequent, alternate) {
-    Object.assign(this, { condition, consequent, alternate })
-  }
-}
-
-export class BinaryExpression {
-  constructor(op, left, right) {
-    Object.assign(this, { op, left, right })
-  }
-}
-
-export class UnaryExpression {
-  constructor(op, operand) {
-    Object.assign(this, { op, operand })
-  }
-}
-
-export class Variable {
-  constructor(name, constant) {
-    Object.assign(this, { name, constant })
-  }
-}
-
-export class Function {
-  constructor(name, numParams, readOnly) {
-    Object.assign(this, { name, numParams, readOnly })
-  }
-}
-
-export const standardLibrary = Object.freeze({
-  pi: new Variable(Math.PI, true),
-  sqrt: new Function('sqrt', 1, true),
-  sin: new Function('sin', 1, true),
-  cos: new Function('cos', 1, true),
-})
