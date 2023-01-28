5ad6a7f5519422bef8d55a7558bfea45125ea851
nklau <63122247+nklau@users.noreply.github.com>
2023-01-27 18:03:36 -0800

    command line script from bella compiler page

---
+++ src/bang.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+
+import fs from "fs/promises"
+import process from "process"
+import compile from "./compiler.js"
+
+const help = `Bang compiler
+
+Syntax: node bella.js <filename> <outputType>
+
+Prints to stdout according to <outputType>, which must be one of:
+
+  analyzed   the semantically analyzed representation
+  optimized  the optimized semantically analyzed representation
+  js         the translation to JavaScript
+`
+
+async function compileFromFile(filename, outputType) {
+  try {
+    const buffer = await fs.readFile(filename)
+    console.log(compile(buffer.toString(), outputType))
+  } catch (e) {
+    console.error(`\u001b[31m${e}\u001b[39m`)
+    process.exitCode = 1
+  }
+}
+
+if (process.argv.length !== 4) {
+  console.log(help)
+} else {
+  compileFromFile(process.argv[2], process.argv[3])
+}
