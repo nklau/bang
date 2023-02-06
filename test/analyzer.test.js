30b3417f298679ab997321d3bd42c2af6bc8d223
nklau <63122247+nklau@users.noreply.github.com>
2023-02-05 18:01:21 -0800

    analyzer test for assignment ops

--- test/analyzer.test.js
+++ test/analyzer.test.js
@@ -65,6 +65,34 @@ const examples = [
     'x + y',
     `   1 | Block statements=[#2]
    2 | BinaryExp left='x' op='+' right='y'`
+  ],
+  [
+    'assignment ops',
+    `local x = 5
+    local const y = 'str'
+    const var = nil
+    local x
+    x += 1.5
+    x.y -= 1.5E4
+    x[y] *= 1e5
+    x[1].y /= 1E-6
+    x.y['str'] %= 1e+4`,
+    `   1 | Block statements=[#2,#3,#4,#5,#6,#7,#9,#11,#14]
+   2 | VariableDec id='x' isLocal=true isReadOnly=false assignmentOp='=' exp=5
+   3 | VariableDec id='y' isLocal=true isReadOnly=true assignmentOp='=' exp='str'
+   4 | VariableDec id='var' isLocal=false isReadOnly=true assignmentOp='=' exp=undefined
+   5 | VariableDec id='x' isLocal=true isReadOnly=false assignmentOp='=' exp=undefined
+   6 | VariableDec id='x' isLocal=false isReadOnly=false assignmentOp='+=' exp=1.5
+   7 | VariableDec id=#8 isLocal=false isReadOnly=false assignmentOp='-=' exp=15000
+   8 | VarSelect id='x' field='y'
+   9 | VariableDec id=#10 isLocal=false isReadOnly=false assignmentOp='*=' exp=100000
+  10 | VarSubscript id='x' selector='y'
+  11 | VariableDec id=#12 isLocal=false isReadOnly=false assignmentOp='/=' exp=0.000001
+  12 | VarSelect id=#13 field='y'
+  13 | VarSubscript id='x' selector=1
+  14 | VariableDec id=#15 isLocal=false isReadOnly=false assignmentOp='%=' exp=10000
+  15 | VarSelect id='x' field=#16
+  16 | VarSubscript id='y' selector='str'`
   ]
 ]
 
