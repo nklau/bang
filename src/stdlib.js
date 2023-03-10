9820f3ec9909365b45abf5521fea72d89c97aff8
nklau <63122247+nklau@users.noreply.github.com>
2023-03-09 22:36:13 -0800

    stdlib objs for add

--- src/stdlib.js
+++ src/stdlib.js
@@ -5,7 +5,12 @@ export const contents = Object.freeze({
   range: new core.Var('range', false, true, ['function']),
   _numLoop: new core.Var('loop', false, true, ['function']),
   _boolLoop: new core.Var('loop', false, true, ['function']),
+  _strLoop: new core.Var('loop', false, true, ['function']),
   _listLoop: new core.Var('loop', false, true, ['function']),
   _objLoop: new core.Var('loop', false, true, ['function']),
-  _strLoop: new core.Var('loop', false, true, ['function'])
+  _numAdd: new core.Var('add', false, true, ['function']),
+  _boolAdd: new core.Var('add', false, true, ['function']),
+  _strConcat: new core.Var('add', false, true, ['function']),
+  _listConcat: new core.Var('add', false, true, ['function']),
+  _objMerge: new core.Var('add', false, true, ['function']),
 })
