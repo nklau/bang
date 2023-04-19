d4de5dd8e8cb606963bb44cf25e407a99da4bf6b
nklau <63122247+nklau@users.noreply.github.com>
2023-04-19 15:01:06 -0700

    fixed tests since compile is async so it can use fs

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,27 +1,22 @@
-import assert from "assert/strict"
-import util from "util"
-import compile from "../src/compiler.js"
+import assert from 'assert/strict'
+import util from 'util'
+import compile from '../src/compiler.js'
 
-const sampleProgram = "print(0)"
+const sampleProgram = 'print(0)'
 
-describe("The compiler", () => {
-  it("throws when the output type is unknown", done => {
-    assert.throws(() => compile("print(0);", "blah"), /Unknown output type/)
-    done()
+describe('The compiler', () => {
+  it('throws when the output type is unknown', async () => {
+    await assert.rejects(
+      async () => await compile('print(0);', 'blah'),
+      /Unknown output type/
+    )
   })
-  it("accepts the analyzed option", done => {
-    const compiled = compile(sampleProgram, "analyzed")
-    assert(util.format(compiled).startsWith("   1 | Block"))
-    done()
+  it('accepts the analyzed option', async () => {
+    const compiled = await compile(sampleProgram, 'analyzed')
+    assert(util.format(compiled).startsWith('   1 | Block'))
   })
-  it("accepts the optimized option", done => {
-    const compiled = compile(sampleProgram, "optimized")
-    assert(util.format(compiled).startsWith("   1 | Block"))
-    done()
+  it('accepts the optimized option', async () => {
+    const compiled = await compile(sampleProgram, 'optimized')
+    assert(util.format(compiled).startsWith('   1 | Block'))
   })
-  // it("generates js code when given the js option", done => {
-  //   const compiled = compile(sampleProgram, "js")
-  //   assert(util.format(compiled).startsWith("console.log(0)"))
-  //   done()
-  // })
 })
