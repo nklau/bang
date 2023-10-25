a891b9b9323d9bec6d725ba5c68def6c02e00236
nklau2016@gmail.com <63122247+nklau@users.noreply.github.com>
2023-10-25 13:31:10 -0700

    added Category enum to core

--- src/core.ts
+++ src/core.ts
@@ -1,3 +1,7 @@
+export enum Category {
+  id
+}
+
 export class Token {
   constructor(category: string, lexeme: string, line: number, column: number) {
     Object.assign(this, { category, lexeme, line, column })
