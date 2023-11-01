11bfabb307787bbbb13bb3b464aee0f6731b361f
nklau2016@gmail.com <63122247+nklau@users.noreply.github.com>
2023-10-31 20:50:59 -0700

    function declarations for parser

---
+++ src/parser.ts
@@ -0,0 +1,22 @@
+import fs from 'fs'
+import { Token } from './core'
+import { tokenize } from './lexer'
+
+export default function parseFile() {
+  if (process.argv.length < 3) {
+    console.log(`Usage: ts-node ${process.argv[1]} <filename.bang>`)
+    process.exit(1)
+  }
+  
+  fs.readFile(process.argv[2], 'utf8', (err, data) => {
+    if (err) {
+      throw err
+    }
+  
+    parse(tokenize(data))
+  })
+}
+
+export const parse = (tokens: Token[]) => {
+  
+}
