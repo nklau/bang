5aafee6730e5e84433482a2624225daae97f9033
nklau2016@gmail.com <63122247+nklau@users.noreply.github.com>
2023-11-28 19:05:28 -0800

    infinity keyword 'inf'

--- src/core/keywords.ts
+++ src/core/keywords.ts
@@ -1,6 +1,7 @@
 export const breakKeyword = 'brk'
 export const constKeyword = 'cst'
 export const falseKeyword = 'F'
+export const infinityKeyword = 'inf'
 export const localKeyword = 'locl'
 export const returnKeyword = 'rtn'
 export const trueKeyword = 'T'
