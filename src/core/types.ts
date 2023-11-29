c98a7bf799500d9671a2d83b67c7f621432eeb96
nklau2016@gmail.com <63122247+nklau@users.noreply.github.com>
2023-11-28 19:05:40 -0800

    number literal class

--- src/core/types.ts
+++ src/core/types.ts
@@ -5,3 +5,7 @@ interface Literal extends Expression {}
 export class BooleanLiteral implements Literal {
   constructor(public value: boolean) {}
 }
+
+export class NumberLiteral implements Literal {
+  constructor(public value: number) {}
+}
