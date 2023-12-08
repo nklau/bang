71ad7ae4c79062826a01a5e7f16e57554642e325
nklau2016@gmail.com <63122247+nklau@users.noreply.github.com>
2023-12-07 18:39:57 -0800

    consolidate equality operators to use more easily in the parser

--- src/core/operators.ts
+++ src/core/operators.ts
@@ -1,7 +1,16 @@
 export const assignmentOperator = '='
+
 export const equalityOperator = '=='
 export const inequalityOperator = '!='
 export const lessThanOperator = '<'
 export const lessThanEqualOperator = '<='
 export const greaterThanOperator = '>'
 export const greaterThanEqualOperator = '>='
+export const equalityOperators = [
+  equalityOperator,
+  inequalityOperator,
+  lessThanOperator,
+  lessThanEqualOperator,
+  greaterThanOperator,
+  greaterThanEqualOperator,
+]
