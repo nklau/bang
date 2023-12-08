1b7226240c0fb508704fafd61d40701a1b5a9d6c
nklau2016@gmail.com <63122247+nklau@users.noreply.github.com>
2023-12-07 20:41:09 -0800

    function declaration for parsing negative (unary) expressions

--- src/parser.ts
+++ src/parser.ts
@@ -354,6 +354,10 @@ export const parse = (tokens: Token[]) => {
     throw new Error('unimplemented')
   }
 
+  const parseNegativeExpression = (): Expression => {
+    throw new Error('unimplemented')
+  }
+
   const parseFunctionLiteral = (expression?: Token[]): FunctionLiteral => {
     throw new Error('unimplemented')
   }
