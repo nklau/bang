527d08abca6df3cd47345a8fbb07ecda8a817452
nklau2016@gmail.com <63122247+nklau@users.noreply.github.com>
2024-03-04 15:39:39 -0800

    unit testing skeleton for parser

---
+++ test/parserTest.ts
@@ -0,0 +1,28 @@
+import assert from 'assert/strict'
+import { parse } from '../src/parser'
+import { Block, Variable, VariableAssignment } from '../src/core/core'
+import { NumberLiteral, StringLiteral } from '../src/core/types'
+import { tokenize } from '../src/lexer'
+
+const programs = [
+  [
+    'number assignment',
+    'x = 5',
+    new Block([new VariableAssignment(new Variable('x', false, false), '=', new NumberLiteral(5))]),
+  ],
+  [
+    'string assignment (single quote)',
+    `x = 'hello world'`,
+    new Block([new VariableAssignment(new Variable('x', false, false), '=', new StringLiteral('hello world'))]),
+  ],
+  [
+    'string assignment (double quote)',
+    `x = "hello world"`,
+    new Block([new VariableAssignment(new Variable('x', false, false), '=', new StringLiteral('hello world'))]),
+  ],
+]
+
+for (const [scenario, program, expected] of programs) {
+  assert.deepEqual(parse(tokenize(program as string)), expected)
+  console.log(`${scenario} passes`)
+}
