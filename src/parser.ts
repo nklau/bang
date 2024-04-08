6ca0023a5b2937e84fca417fa8ff55f50d2634cb
nklau2016@gmail.com <63122247+nklau@users.noreply.github.com>
2024-04-08 13:14:40 -0700

    parseFile function takes a file name as input instead of reading from process.argv

--- src/parser.ts
+++ src/parser.ts
@@ -76,13 +76,8 @@ import {
 } from './core/types'
 import { tokenize } from './lexer'
 
-export default function parseFile() {
-  if (process.argv.length < 3) {
-    console.log(`Usage: ts-node ${process.argv[1]} <filename.bang>`)
-    process.exit(1)
-  }
-
-  fs.readFile(process.argv[2], 'utf8', (err, data) => {
+export default function parseFile(fileName: string) {
+  fs.readFile(fileName, 'utf8', (err, data) => {
     if (err) {
       throw err
     }
